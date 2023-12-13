969068d52b6e2148c7a88f5681a4326a12200b69
Shunta Mitani <211140093c@ed.fuk.kindai.ac.jp>
2023-12-13 08:55:49 +0000

    ログイン画面から素材一覧への移動

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -10,7 +10,8 @@ class StocksController < ApplicationController
   
   def create
     logger.debug params[:stock][:content]
-    @stock = Stock.create params.require(:stock).permit(:content, :image) # POINT
+    #@stock = Stock.create params.require(:stock).permit(:content, :image) # POINT
+    @stock = Stock.new(stock_params)
     redirect_to @stock
     #@stock = Stock.create params.require(:stock).permit(:content)
     #if image = params[:stock][:image]
@@ -19,7 +20,7 @@ class StocksController < ApplicationController
   end
   
   def show
-    @stock = Stock.find(params[:id])
+    #@stock = Stock.find(params[:id])
   end
   
   def edit
@@ -39,7 +40,7 @@ class StocksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def stock_params
-      params.require(:stock).permit(:title, :image)
+      params.require(:stock).permit(:content, :image)
     end
   
   
@@ -48,72 +49,3 @@ class StocksController < ApplicationController
     #params.require(:stock).permit(:content, :image)
   #end
 end
-
-  before_action :set_article, only: %i[ show edit update destroy ]
-
-  # GET /articles or /articles.json
-  def index
-    @articles = Article.all
-  end
-
-  # GET /articles/1 or /articles/1.json
-  def show
-  end
-
-  # GET /articles/new
-  def new
-    @article = Article.new
-  end
-
-  # GET /articles/1/edit
-  def edit
-  end
-
-  # POST /articles or /articles.json
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /articles/1 or /articles/1.json
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /articles/1 or /articles/1.json
-  def destroy
-    @article.destroy
-
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:title, :image)
-    end
